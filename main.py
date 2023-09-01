8603b19a46a2f0a47071a2aebd1babe06370257f
MilanJanko <miljankovic91@gmail.com>
2023-09-01 11:04:41 +0200

    Added search menu and success message

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
-    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, \
+    QDialog, QComboBox, QMessageBox
 from PyQt6.QtGui import QAction
 import sqlite3
+from PyQt6.QtCore import Qt, QTimer
 
 
 class MainWindow(QMainWindow):
@@ -12,6 +14,7 @@ class MainWindow(QMainWindow):
 
         file_menu_item = self.menuBar().addMenu('&File')
         help_menu_item = self.menuBar().addMenu('&Help')
+        edit_menu_item = self.menuBar().addMenu('&Edit')
 
         add_student_action = QAction('Add Student', self)
         add_student_action.triggered.connect(self.insert)
@@ -19,6 +22,10 @@ class MainWindow(QMainWindow):
 
         about_action = QAction('About', self)
         help_menu_item.addAction(about_action)
+
+        search_action = QAction('Search', self)
+        search_action.triggered.connect(self.search)
+        edit_menu_item.addAction(search_action)
 
         self.table = QTableWidget()
         self.table.setColumnCount(4)
@@ -40,6 +47,10 @@ class MainWindow(QMainWindow):
     def insert(self):
         dialog = InsertDialog()
         dialog.exec()
+
+    def search(self):
+        self.dialog = SearchDialog()
+        self.dialog.exec()
 
 
 class InsertDialog(QDialog):
@@ -82,6 +93,51 @@ class InsertDialog(QDialog):
         cursor.close()
         connection.close()
         student.load_data()
+        success_message = QMessageBox()
+        success_message.setWindowTitle('Success')
+        success_message.setText('You successfully added new student!')
+        success_message.exec()
+        QTimer.singleShot(1000, self.accept)
+
+
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Search Student')
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit()
+        layout.addWidget(self.student_name)
+
+        search_button = QPushButton('Search')
+        search_button.clicked.connect(self.search_student)
+        layout.addWidget(search_button)
+
+        self.setLayout(layout)
+
+    def search_student(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        result = cursor.execute('SELECT * FROM students WHERE name = ?', (name,))
+        rows = list(result)
+        items = student.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            student.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
+        success_message = QMessageBox()
+        success_message.setWindowTitle(f'{len(items)} Records Found')
+        success_message.setText('This window will close automatically after 2 seconds')
+        timer = QTimer(self)
+        timer.timeout.connect(success_message.accept)
+        timer.start(1500)
+        success_message.exec()
+        QTimer.singleShot(1000, self.accept)
 
 
 app = QApplication(sys.argv)
